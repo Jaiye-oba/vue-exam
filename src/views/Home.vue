a72dd7801440281e5d55e11dfa49a0226d4f0727
Jaiyeoba Funmilayo <jaiyeobaobiageli@gmail.com>
2023-03-17 21:33:33 +0100

    vue exam

---
+++ src/views/Home.vue
@@ -0,0 +1,60 @@
+<script setup>
+import {RouterLink} from 'vue-router'
+import img from '../assets/png.png'
+</script>
+
+<template>
+    <div class="h-screen md:h-screen wrapper flex gap-4 flex-col justify-between items-start ">
+        <nav><h1 class="text-2xl text-black font-extrabold px-6"></h1></nav>
+       <div class="grid grid-cols-2  px-3 py-5">
+            <div class="col-span-2 md:col-span-1">
+                <header class="flex flex-col justify-center items-start px-3 gap-4">
+                    <h1 class="text-blue-500 text-4xl font-bold">No one stops you from counting</h1>
+                    <p class="font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere est blanditiis maiores minus maxime at similique. In, consequuntur excepturi praesentium modi aut autem facere voluptatibus!
+                    </p>
+                    <div class="hidden md:block">
+                        <h1 class="text-blue-500 text-4xl font-bold">Get to see my counter developed with Vue</h1>
+                    <p class="font-light"> A very simple app that has a host of dependable functionalities
+                    </p>
+                    <ul class="ml-10 mt-4 ">
+                        <li class="text-xl">Increment</li>
+                        <li class="text-xl">Decrement</li>
+                        <li class="text-xl">Reset counter Value</li>
+                    </ul>
+
+                    <h2 class="text-2xl mt-2">...elegance in the making</h2>
+                    </div>
+                    <RouterLink to="/counter">
+                        <button class="px-3 py-2 bg-blue-500 text-white rounded-md border border-blue-500"> See Counter</button>
+                    </RouterLink>
+                </header>
+            </div>
+
+            <div class="col-span-1 png hidden md:block">
+                <img :src="img" alt="png image" />
+            </div>
+       </div>
+       <div>
+        <div class="w-10/12 px-2 rounded-md py-3 shadow-slate-200 shadow-md mb-4 mx-auto flex flex-col justify-center gap-4 items-center">
+            <h1 class="text-2xl text-blue-500 font-bold"> Counter App</h1>
+            <p class="text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. In ducimus voluptatibus maiores eos quia voluptas corrupti ipsa nostrum commodi ratione, natus quos eveniet dicta totam quod? Facilis, impedit consequuntur dolorem recusandae fuga ipsa? Illum, quaerat.</p>
+           <RouterLink to="/counter"> <button class="px-3 py-2 bg-blue-500 text-white rounded-md border border-blue-500"> See in action</button> </RouterLink>
+        </div>
+    </div>
+    </div>
+
+    
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@1,700&display=swap');
+.wrapper{
+    background-image: url('../assets/bg.jpeg');
+    background-size:cover;
+    font-family: 'Alegreya', serif;
+}
+.png{
+
+}
+
+</style>
---
+++ src/views/counter.vue
@@ -0,0 +1,47 @@
+<script setup>
+import useCounter from '../composable/useCount.js'
+import {ref} from 'vue'
+
+let num = ''
+const {increment,decrement,reset,setValue,number} = useCounter(num)
+</script>
+
+<template>
+    <div class="h-screen counter w-full flex flex-col gap-5 justify-center items-center">
+        <h1 class="text-4xl font-bold text-blue-500 ">Counter Page</h1> 
+        <div class="">
+            <input type="text" class=" border border-sltae-200 rounded-md h-10 py-4 outline-none px-3 shadow-md shadow-slate-200" placeholder="set value" v-model="num" />
+            <button  @click="setValue(num)" class="bg-blue-500 text-white border border-blue-500 px-3 py-3 rounded-md font-extrabold text-xl">Add</button>
+        </div>
+        
+        <div class="">
+            <h1 class="text-4xl font-bold text-blue-500"> {{ number }}</h1>
+        </div>
+        
+        <div>
+            <div class="flex justify-center items-center gap-5">
+                    <button @click="decrement" class="bg-blue-500 text-white px-2 rounded-ms  py-3 text-xl font-bold">Minus</button>
+                    <button @click="reset" class="bg-red-500 text-white px-2 rounded-ms  py-3 text-xl font-bold">Reset</button>
+                    <button @click="increment" class="bg-blue-500 text-white px-2 rounded-ms  py-3 text-xl font-bold">Add</button>
+            </div>
+        </div> 
+
+            <router-link to='/' class="mt-7" >
+                <button class="px-3 py-2 bg-green-500 text-white rounded-md border border-blue-500">
+                    Return Home
+                </button>
+            </router-link>
+    </div>
+</template>
+
+<style scoped>
+
+    .counter{
+    background-image: url('../assets/bg.jpeg');
+    background-size:cover;
+    font-family: 'Alegreya', serif;
+
+    }
+    
+
+</style>
